0c47f8afa528104f157669aff113f7651a1c32e5
Kevin Peter Flores Tarmeño <kflorest@everis.com>
2019-11-07 16:50:25 -0500

    API Gateway 7/11/2019

---
+++ Gateway/src/main/java/com/sistema/app/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
